d2557029d36de7985f76bb679bb2879e21da2218
victor51001 <victormangil2@gmail.com>
2023-11-17 13:44:29 +0100

    Automated commit - 2023-11-17 13:44:15

---
+++ auto_push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Obtener la fecha y hora actual
+current_datetime=$(date '+%Y-%m-%d %H:%M:%S')
+
+# Git add
+git add .
+
+# Git commit con fecha y hora actual
+git commit -m "Automated commit - $current_datetime"
+
+# Git push con nombre de usuario y token
+git push "https://github.com/victor51001/PracticasProcesos.git"
+git push 
+
+git status 
+
+sleep 5
