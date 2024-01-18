e443a1cb3431bd953d6a8b24582560a632d93593
victor51001 <victormangil2@gmail.com>
2024-01-18 13:19:23 +0100

    Automated commit - 2024-01-18 13:19:23

--- AdivinarNumeroSocketUDP/src/Servidor/Servidor.java
+++ AdivinarNumeroSocketUDP/src/Servidor/Servidor.java
@@ -1,10 +1,31 @@
 package Servidor;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Random;
+
 public class Servidor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			Random r = new Random();
+			int numero = r.nextInt(0,101);
+			while (true) {
+				DatagramSocket socket = new DatagramSocket();
+				
+				byte[] buffer = new byte[256];
+				
+				DatagramPacket paquete = new DatagramPacket(buffer, buffer.length);
+				socket.receive(paquete);
+				int intento = Integer.parseInt(new String(paquete.getData()).trim());
+			}
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
